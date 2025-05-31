c064580ecf834fc81388c8894a64112fe84bb72a
audrey <audreyreal@proton.me>
2025-05-31 13:28:43 -0600

    make apply to wa formdata type more explicit

--- src/networking/html/types.ts
+++ src/networking/html/types.ts
@@ -11,7 +11,8 @@ type MoveRegionFormData = {
  * Type representing the formdata for applying to the World Assembly.
  */
 type ApplyToWorldAssemblyFormData = {
-    action: "join_UN";
-    submit?: "1";
-    resend?: "1";
-}
+  action: "join_UN";
+} & (
+  | { submit: "1"; resend?: never } // Exactly `submit`, no `resend`
+  | { resend: "1"; submit?: never } // Exactly `resend`, no `submit`
+);
