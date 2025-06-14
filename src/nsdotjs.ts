97a371e8a283e256ecf28ed01faf16159e14af1c
audrey <audreyreal@proton.me>
2025-06-13 19:47:05 -0600

    fix x2

--- src/nsdotjs.ts
+++ src/nsdotjs.ts
@@ -2,6 +2,12 @@ import { NSScript } from "./client";
 import { addTopBarButton } from "./gui/topBarButton";
 import { addSidebarButton } from "./gui/sideBarButton";
 import { prettify, canonicalize } from "./helpers";
+// imported everything for assigning to window
+// now exporting for docs
+export { NSScript } from "./client";
+export { addTopBarButton } from "./gui/topBarButton";
+export { addSidebarButton } from "./gui/sideBarButton";
+export { prettify, canonicalize } from "./helpers";
 export type { StatusBubble } from "./gui/statusBubble";
 // export all types from both types files
 export type * from "./networking/html/types";
